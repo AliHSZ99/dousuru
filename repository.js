0de226cb48d64e47caeb069546cd5f16dca1ab94
airi <sakutaidf@gmail.com>
2021-05-12 15:30:15 -0400

    fixed bar and fixed sizes of pages

--- repository.js
+++ repository.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
         $("#projectDescriptionBox > p").html("This ATM MACHINE is our very first project together.");
         $("img").attr("src", "/imgs/jav.png");
         $("#downloadableFile").attr("href", "ATM.zip");
+        $("#projectTitle").css("left", "280px")
     });
 
     $("#button2").click(function(){
@@ -14,6 +15,7 @@ $(document).ready(function(){
         " have to break through walls to get power ups. Players have to tactically move around the map picking up these power ups to defeat other players and move on to the next round of the first to three.");
         $("#projectImg").attr("src", "/imgs/game.png");
         $("#downloadableFile").attr("href", "GameProject.zip");
+        $("#projectTitle").css("left", "260px")
     });
 
     $("#button3").click(function(){
@@ -24,6 +26,7 @@ $(document).ready(function(){
         + "After doing so, we populated them  and performed 6 specific queries on them. Finally, we created a report "
         + "containing our database schema, normalization of our tables, and the queries results.");
         $("#downloadableFile").attr("href", "FinalProjectDB.zip");
+        $("#projectTitle").css("left", "200px")
     });
 
     $("#button4").click(function(){
@@ -32,6 +35,7 @@ $(document).ready(function(){
         ". A CAD/CAM PC is a software used to design and manufacture prototypes, finished products, and production runs of products."); 
         $("img").attr("src", "/imgs/cad.png");
         $("#downloadableFile").attr("href", "cad.pptx");
+        $("#projectTitle").css("left", "150px")
     });
 
     $("#button5").click(function(){
@@ -41,5 +45,6 @@ $(document).ready(function(){
          "It provides real transaction features such as selecting different kinds of items and getting rewards for being a loyal customer."); 
         $("img").attr("src", "/imgs/cafe.png");
         $("#downloadableFile").attr("href", "RestaurantApplication-main.zip");
+        $("#projectTitle").css("left", "100px")
     });
 });
