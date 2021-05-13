67d5a67d3ae906448d8cbb596eeb6782d7dce290
Vincent Benesen <you@example.com>
2021-05-12 23:35:09 -0400

    fixed minor issues

--- ali.js
+++ ali.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         $("#description").html("The Vanier School system was my final project for the Programming 2 course. It was done using JAVA Swing. Its " +
         "goal was to make an OMNIVOX LEA similutor. We had to do so by creating a Login frame, an Admin frame, a Student frame " +
         "and a Teacher frame.");
-        $("#project-img").attr("src", "/imgs/p2img.png");
+        $("#project-img").attr("src", "imgs/p2img.png");
         $("#downloadable-file").attr("href", "prog2FinalProjectAli.zip");
     });
 
@@ -13,7 +13,7 @@ $(document).ready(function(){
         $("#title").html("Ali Zoubeidi's Website");
         $("#description").html("This is the first website I have ever created using only HTML and CSS. It is a site called Ali Zoubeidi's " +
         "website and it is all about me. This website includes facts about my life and my professional goals. Download to find out more!");
-        $("#project-img").attr("src", "/imgs/ali'swebsite.png");
+        $("#project-img").attr("src", "imgs/ali'swebsite.png");
         $("#downloadable-file").attr("href", "AliZoubeidiWebsite.zip");
     });
 
@@ -22,7 +22,7 @@ $(document).ready(function(){
         $("#description").html("Ever heard of WAR? It is a simple luck card game. The player and the dealer each draw a card from the deck, and " +
         "the player with the highest card value gets a point. If the players have the same card value, 3 extra cards are drawn and the last one of the 3 " +
         "cards are compared. The match finishes when the deck is empty. Download to play!");
-        $("#project-img").attr("src", "/imgs/war.png");
+        $("#project-img").attr("src", "imgs/war.png");
         $("#downloadable-file").attr("href", "WarAli.zip");
     });
 
@@ -31,7 +31,7 @@ $(document).ready(function(){
         $("#description").html("Here is my first work on cookies. I had to send information to an API to retrieve a token when the email and password " +
         "values were correct. I am mainly displaying this work here not because of the cookies aspect, but to show case my clean and simple design. Please " +
         "feel free to download this work to take a look at my login page design.");
-        $("#project-img").attr("src", "/imgs/cookies.png");
+        $("#project-img").attr("src", "imgs/cookies.png");
         $("#downloadable-file").attr("href", "AliCookiesAssignment.zip");
     });
 
--- repository.js
+++ repository.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $("#button1").click(function(){
         $("#projectTitle").html("ATM MACHINE");
         $("#projectDescriptionBox > p").html("This ATM MACHINE is our very first project together.");
-        $("img").attr("src", "/imgs/jav.png");
+        $("img").attr("src", "imgs/jav.png");
         $("#downloadableFile").attr("href", "ATM.zip");
         $("#projectTitle").css("left", "280px")
     });
@@ -13,14 +13,14 @@ $(document).ready(function(){
         $("#projectDescriptionBox > p").html("An exciting game for everyone. People can play with their family, "+
         "friends and even random strangers that are willing to compete. Robotic bugs of all kinds are put inside different mazes and"+
         " have to break through walls to get power ups. Players have to tactically move around the map picking up these power ups to defeat other players and move on to the next round of the first to three.");
-        $("#projectImg").attr("src", "/imgs/game.png");
+        $("#projectImg").attr("src", "imgs/game.png");
         $("#downloadableFile").attr("href", "GameProject.zip");
         $("#projectTitle").css("left", "260px")
     });
 
     $("#button3").click(function(){
         $("#projectTitle").html("Dentist Database");
-        $("img").attr("src", "/imgs/db.png");
+        $("img").attr("src", "imgs/db.png");
         $("#projectDescriptionBox > p").html("In this project, we created a dentist database from scratch. "
         + "We created tables such as Patient, Appointment, Dentist, Visit, VisitDetails, Billing, and Patient_Payment. " 
         + "After doing so, we populated them  and performed 6 specific queries on them. Finally, we created a report "
@@ -33,7 +33,7 @@ $(document).ready(function(){
         $("#projectTitle").html("CAD/CAM PC Build");
         $("#projectDescriptionBox > p").html("In this project, we are responsilbe for building a CAD/CAM PC "+
         ". A CAD/CAM PC is a software used to design and manufacture prototypes, finished products, and production runs of products."); 
-        $("img").attr("src", "/imgs/cad.png");
+        $("img").attr("src", "imgs/cad.png");
         $("#downloadableFile").attr("href", "cad.pptx");
         $("#projectTitle").css("left", "150px")
     });
@@ -43,7 +43,7 @@ $(document).ready(function(){
         $("#projectDescriptionBox > p").html("The Problem Solver’s Restaurant is a GUI Application that is used to order food. This restaurant app is designed to be a " +
          "user-friendly application that will allow users to order great food from a specific restaurant. " +
          "It provides real transaction features such as selecting different kinds of items and getting rewards for being a loyal customer."); 
-        $("img").attr("src", "/imgs/cafe.png");
+        $("img").attr("src", "imgs/cafe.png");
         $("#downloadableFile").attr("href", "RestaurantApplication-main.zip");
         $("#projectTitle").css("left", "100px")
     });
